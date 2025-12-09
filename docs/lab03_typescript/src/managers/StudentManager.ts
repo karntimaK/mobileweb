543d91409d8cc4232ac3ec3c29e00b52a72ab54d
karntima khampee <karntima.k@kkumail.com>
2025-12-10 02:19:30 +0700

    Lab03_typescript

---
+++ docs/lab03_typescript/src/managers/StudentManager.ts
@@ -0,0 +1,60 @@
+import { Student } from "../models/Student.js";
+
+
+export class StudentManager {
+  private students: Student[] = [];
+
+
+  addStudent(student: Student): void {
+    this.students.push(student);
+    this.saveToLocalStorage();
+  }
+
+
+  getAllStudents(): Student[] {
+    return this.students;
+  }
+
+
+  findStudentByID(id: string): Student | undefined {
+    return this.students.find(s => s.id === id);
+  }
+
+
+  findStudentsByName(keyword: string): Student[] {
+    const lower = keyword.toLowerCase();
+
+
+    return this.students.filter(s =>
+    s.first_name.toLowerCase().includes(lower) ||
+    s.last_name.toLowerCase().includes(lower)
+    );
+  }
+
+
+  findStudentByEmail(email: string): Student[] {
+    return this.students.filter(s =>
+      s.email.toLowerCase().includes(email.toLowerCase())
+    );
+  }
+
+
+  findStudentsByMajor(major: string): Student[] {
+    return this.students.filter(s =>
+      s.major.toLowerCase().includes(major.toLowerCase())
+    );
+  }
+
+
+  saveToLocalStorage(): void {
+    localStorage.setItem("students", JSON.stringify(this.students));
+  }
+
+
+  loadFromLocalStorage(): void {
+    const data = localStorage.getItem("students");
+    if (data) {
+      this.students = JSON.parse(data);
+    }
+  }
+}
